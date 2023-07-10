c8dd6715c36c44a6c9db0bba136c513af1f59013
alfian.yuska <alfian.yuska@phiraka.com>
2023-07-10 11:36:12 +0700

    updateApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
 
+    const [data, setData] = useState([])
+
     useEffect(() => {
-        axios.get('https://reqres.in/api/users?page=2')
+        axios.delete('https://reqres.in/api/users/2'
+        )
             .then(res => console.log(res))
             .catch(err => console.log(err));
     }, [])
@@ -12,6 +15,11 @@ function App() {
     return (
         <div>
             <h2>Axios library in React Js</h2>
+            {/* {
+                data.map((d, i) => {
+                    return <p key={i}>{d.email}</p>
+                })
+            } */}
         </div>
     )
 }
